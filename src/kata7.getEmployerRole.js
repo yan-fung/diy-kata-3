d9458fbd3e593b9464704db1a0819320d3896925
Yan Fung <fungmingyan@hotmail.com>
2022-10-12 18:00:00 +0100

    completed getEmployeerRole

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,6 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+  const staff = employees.find((person) => person.name === employeeName);
+  return staff.role;
+};
 
 module.exports = getEmployerRole;
--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,23 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  test("returns the employee's role in the company", () => {
+    const employees = [
+      {
+        name: "Satti",
+        role: "Developer",
+      },
+      {
+        name: "Jenny",
+        role: "Sales Associate",
+      },
+      {
+        name: "Javid",
+        role: "Human Recommended Reading Assistant",
+      },
+    ];
+
+    expect(getEmployerRole('Javid', employees)).toEqual('Human Recommended Reading Assistant');
+    expect(getEmployerRole('Jenny', employees)).toEqual('Sales Associate');
+  });
 });
